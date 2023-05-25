cc0e99dfe22a372f9aa5853dcc95306830dd8199
pumukun <demgrisha@gmail.com>
2023-05-25 17:48:59 +0300

    fixed issues in AST

--- include/AST.hpp
+++ include/AST.hpp
@@ -8,73 +8,87 @@
 
 using namespace std;
 
-class Expression_Node;
+class Expression_Node {
+public:
+	virtual ~Expression_Node() = default;
+};
 
 
-class Statsmest_Node : public Expression_Node{
+class Statsment_Node : public Expression_Node {
 public:
 	vector<Expression_Node> MyNode;
 
-	void Add_Node(Expression_Node p_node){
+	void Add_Node(Expression_Node p_node) {
 		MyNode.push_back(p_node);
 	}
-}
 
-class Variable_Node : public Expression_Node{
+	~Statsment_Node() = default;
+}; 
+
+class Variable_Node : public Expression_Node {
 public: 
 	 Token token;
 
 	 Variable_Node(Token p_token) {
 	 	token = p_token;
 	 }
-}
+
+	 ~Variable_Node() = default;
+};
 
 
-class Number_Node : public Expression_Node{
+class Number_Node : public Expression_Node {
 public:
 	Token number;
 
-	Number_Node(Token p_number){
+	Number_Node(Token p_number) {
 		number = p_number;
 	}
-}
 
-class Binary_oper_Node : public Expression_Node{
+	~Number_Node() = default;
+};
+
+class Binary_oper_Node : public Expression_Node {
+public:
+	Token oper;
+	Expression_Node* left_node;
+	Expression_Node* right_node;
+
+	Binary_oper_Node(Token p_oper, Expression_Node* p_left_node, Expression_Node* p_right_node) {
+		oper = p_oper;
+		left_node = p_left_node;
+		right_node = p_right_node;
+	}
+
+	~Binary_oper_Node() = default;
+};
+
+class Unar_oper_Node : public Expression_Node {
+public:
+	Token oper;
+	Expression_Node *operand;
+
+	Unar_oper_Node(Token p_oper, Expression_Node *p_operand) {
+		oper = p_oper;
+		operand = p_operand;
+	}
+
+	~Unar_oper_Node() = default;
+};
+
+class Assign_Node : public Expression_Node {
 public:
 	Token oper;
 	Expression_Node *left_node;
 	Expression_Node *right_node;
 
-	Binary_oper_Node(Token p_oper, Expression_node *p_left_node,Expression_node *p_right_node){
+	Assign_Node(Token p_oper, Expression_Node *p_left_node, Expression_Node *p_right_node) {
 		oper = p_oper;
 		left_node = p_left_node;
 		right_node = p_right_node;
 	}
 
-}
-
-class Unar_oper_Node : public Expression_Node{
-public:
-	Token oper;
-	Expression_Node *operand;
-
-	Unar_oper_Node(Token p_oper, Expression_node *p_operand){
-		oper = p_oper;
-		operand = p_operand;
-	}
-}
-
-class Assign_Node : public Expression_Node{
-public:
-	Token oper;
-	Expression_node *left_node;
-	Expression_Node *right_node;
-
-	Assign_Node(Token p_oper, Expression_node *p_left_node, Expression_node *p_right_node){
-		oper = p_oper;
-		left_node = p_left_node;
-		right_node = p_right_node;
-	}
-}
+	~Assign_Node() = default;
+};
 
 #endif
--- test/testast.cpp
+++ test/testast.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
 #include "../include/AST.hpp"
+#include "../include/Token.hpp"
+
 
 using namespace std;
 
 int main() {
-	
+	Token a;
+	a.set_lexeme("a");
+	a.set_pos(0);
+	a.set_type(VARIABLE);
+	Variable_Node nv(a);
 	
 	return 0;
 }
