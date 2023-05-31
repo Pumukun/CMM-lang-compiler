a9b195262f9118b618ca55f11d70d70e5b7c8bec
wayflayer <wayflayeeer@gmail.com>
2023-05-31 18:23:11 +0300

    измененное и отредактированное АСТ дерево

--- include/AST.hpp
+++ include/AST.hpp
@@ -1,128 +1,46 @@
 #ifndef AST_HPP
 #define AST_HPP
 
+#include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 
 #include "Token.hpp"
 
 using namespace std;
 
-/*
-
-class Expression_Node {
-public:
-	virtual ~Expression_Node() = default;
-};
-
-
-class Statesment_Node : public Expression_Node {
-public:
-	vector<Expression_Node> MyNode;
-
-	void add_node(Expression_Node p_node) {
-		MyNode.push_back(p_node);
-	}
-
-	~Statesment_Node() = default;
-}; 
-
-class Variable_Node : public Expression_Node {
-public: 
-	 Token token;
-
-	 Variable_Node(Token p_token) {
-	 	token = p_token;
-	 }
-
-	 ~Variable_Node() = default;
-};
-
-
-class Number_Node : public Expression_Node {
-public:
-	Token number;
-
-	Number_Node(Token p_number) {
-		number = p_number;
-	}
-
-	~Number_Node() = default;
-};
-
-class Binary_oper_Node : public Expression_Node {
-public:
-	Token oper;
-	Expression_Node left_node;
-	Expression_Node right_node;
-
-	Binary_oper_Node(Token p_oper, Expression_Node p_left_node, Expression_Node p_right_node) {
-		oper = p_oper;
-		left_node = p_left_node;
-		right_node = p_right_node;
-	}
-
-	~Binary_oper_Node() = default;
-};
-
-class Unar_oper_Node : public Expression_Node {
-public:
-	Token oper;
-	Expression_Node operand;
-
-	Unar_oper_Node(Token p_oper, Expression_Node p_operand) {
-		oper = p_oper;
-		operand = p_operand;
-	}
-
-	~Unar_oper_Node() = default;
-};
-
-class Assign_Node : public Expression_Node {
-public:
-	Token oper;
-	Expression_Node *left_node;
-	Expression_Node *right_node;
-
-	Assign_Node(Token p_oper, Expression_Node *p_left_node, Expression_Node *p_right_node) {
-		oper = p_oper;
-		left_node = p_left_node;
-		right_node = p_right_node;
-	}
-
-	~Assign_Node() = default;
-};
-
-#endif
-*/
-
-
 class Extra_node{
 private:
 	Token token;
+	Token oper;
 	Extra_node* left_node;
 	Extra_node* right_node;	
 public:
-	void set_token(token){
-		this->token = token;
-		p_token;
+	void set_oper(Token p_oper){
+		p_oper = oper;
 	}
-	void set_left_node(left_node){
-		this->left_node = left_node;
+	void set_token(Token p_token){
+		p_token = token;
 	}
-	void set_right_node(right_node){
-		this->right_node = right_node;
+	void set_left_node(Extra_node* p_left_node){
+		p_left_node = left_node;
 	}
-	get_token() {return token;}
-	get_left_token() {return left_node;}
-	get_right_token() {return right_node;}
+	void set_right_node(Extra_node* p_right_node){
+		p_right_node = right_node;
+	}
+
+	Token get_oper() {return oper;}
+	Token get_token() {return token;}
+	Extra_node* get_left_node() {return left_node;}
+	Extra_node* get_right_node() {return right_node;}
 	Extra_node() = default;
 	
-	Extra_Node(Token p_token) {
+	void Extra_Node(Token p_token) {
 	 	token = p_token;
 	}
 
-	Extra_Node(Token p_oper, Expression_Node p_left_node, Expression_Node p_right_node) {
+	void Extra_Node(Token p_oper, Extra_node* p_left_node, Extra_node* p_right_node) {
 		oper = p_oper;
 		left_node = p_left_node;
 		right_node = p_right_node;
@@ -131,9 +49,13 @@ public:
 	~Extra_node() = default;
 	vector <Extra_node> nodes;
 
-	void add_node(Extra_Node p_node) {
+	void add_node(Extra_node p_node) {
 		nodes.push_back(p_node);
 	}
-}
+	
+
+
+
+};
 
 #endif
