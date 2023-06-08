f398122e4d93fa3c187ab892838cff0dd2ce54fa
pumukun <demgrisha@gmail.com>
2023-06-08 20:18:17 +0300

    changed pos to index

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -19,7 +19,7 @@ public:
 
 	Parser(vector<Token> p_tokens):
 		tokens(move(p_tokens)), 
-		pos(0)  
+		index(0)  
 	{ }
 
 	~Parser() = default;
@@ -27,17 +27,17 @@ public:
 private:
 
 	vector<Token> tokens;
-	int pos;
+	int index;
 	map<string, int> scope_int;
 	map<string, string> scope_str;
 
 private:
 
 	Token match(TokenType p_expected) {
-		if (pos < (int)tokens.size()) {
-			const Token cur_Token = tokens[pos];
+		if (index < (int)tokens.size()) {
+			const Token cur_Token = tokens[index];
 			if (p_expected == cur_Token.get_type()) {
-				pos++;
+				index++;
 				return cur_Token;
 			}
 		}
@@ -47,27 +47,27 @@ private:
 	Token require(TokenType p_expected) {
 		const Token token = match(p_expected);
 		if (token.get_type() == END) { 
-			throw runtime_error("expected: " + TokenType_array[p_expected] + " on position: " + to_string(tokens[pos].get_pos()) + "\n");
+			throw runtime_error("expected: " + TokenType_array[p_expected] + " on position: " + to_string(tokens[index].get_pos()) + "\n");
 		}
 		return token;
 	}
 	
 	AST_Node* parse_formula() {
-		Token tmp_Token = tokens[pos];
+		Token tmp_Token = tokens[index];
 		int tmp_type = tmp_Token.get_type();
 		
 		string formula = "";
 
 		while (tmp_type == OPERATOR || tmp_type == VARIABLE || tmp_type == INTEGER || tmp_type == PUNCTUATION) {
-			tmp_Token = tokens[pos]; 
+			tmp_Token = tokens[index]; 
 			tmp_type = tmp_Token.get_type();
 			if (tmp_type == SEMICOLON) break;
 			formula = formula + tmp_Token.get_lexeme();
-			pos++;
+			index++;
 		}
 
-		cout << formula << "\n" << pos << endl;
-		pos++;
+		cout << formula << "\n" << index << endl;
+		index++;
 		
 		for (auto const &i : scope_int) {
 			int tmp_find = formula.find(i.first);
@@ -83,17 +83,17 @@ private:
 	}
 
 	AST_Node* parse_print() {
-		cout << "print " + tokens[pos].get_lexeme() << endl;
-		pos++;
+		cout << "print " + tokens[index].get_lexeme() << endl;
+		index++;
 		require(SEMICOLON);
-		return new AST_Node(tokens[pos]);
+		return new AST_Node(tokens[index]);
 	}
 
 	AST_Node* parse_var() {
-		if (tokens[pos].get_type() == OPERATOR && tokens[pos].get_lexeme() != "-")
-			throw runtime_error("expected \"-\" on pos: " +  to_string(tokens[pos].get_pos()));
-		AST_Node* res = new AST_Node(tokens[pos]);
-		pos++;
+		if (tokens[index].get_type() == OPERATOR && tokens[index].get_lexeme() != "-")
+			throw runtime_error("expected \"-\" on index: " +  to_string(tokens[index].get_pos()));
+		AST_Node* res = new AST_Node(tokens[index]);
+		index++;
 		return res;
 	}
 
@@ -102,18 +102,18 @@ public:
 	AST_Node* parse_code() {
 		AST_Node* root = new AST_Node();
 
-		while (pos < (int)tokens.size()) {
+		while (index < (int)tokens.size()) {
 			if (match(VARIABLE).get_type() != END) {
-				string tmp_var = tokens[pos - 1].get_lexeme();
+				string tmp_var = tokens[index - 1].get_lexeme();
 
 				require(ASSIGN);
 				
 				
-				if (tokens[pos - 1].get_lexeme() != "=")
-					throw runtime_error("expected: = after variable! on position: " + to_string(tokens[pos].get_pos()));
+				if (tokens[index - 1].get_lexeme() != "=")
+					throw runtime_error("expected: = after variable! on position: " + to_string(tokens[index].get_pos()));
 
-				if (tokens[pos].get_type() == INTEGER) {
-					string tmp_str = tokens[pos].get_lexeme();
+				if (tokens[index].get_type() == INTEGER) {
+					string tmp_str = tokens[index].get_lexeme();
 
 					AST_Node* f_node = new AST_Node();
 					if ((int)tmp_str.find("-") != -1 || (int)tmp_str.find("-") == 0 || (int)tmp_str.find("/") != -1 || (int)tmp_str.find("*") != -1) {
@@ -123,21 +123,20 @@ public:
 
 					scope_int[tmp_var] = stoi(f_node->get_token().get_lexeme());
 					insert_right(root, f_node);
-				} else if (tokens[pos].get_type() == STRING) {
-					pos++;
-					scope_str[tmp_var] = tokens[pos].get_lexeme();
+				} else if (tokens[index].get_type() == STRING) {
+					index++;
+					scope_str[tmp_var] = tokens[index].get_lexeme();
 				}
 			}
 			
 			if (match(PRINT).get_type() != END) {
-				if (tokens[pos].get_type() == VARIABLE || tokens[pos].get_type() == INTEGER || tokens[pos].get_type() == STRING)
+				if (tokens[index].get_type() == VARIABLE || tokens[index].get_type() == INTEGER || tokens[index].get_type() == STRING)
 					parse_print();
 				else throw runtime_error("expected: VAR || INT || STR after print!");
 			}
 
-			if (tokens[pos].get_type() == END) return root;
+			if (tokens[index].get_type() == END) return root;
 		}
-
 		return root;
 	}
 };
